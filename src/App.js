0ede70554bfda2dcdb16049ac502e30477b9174a
Hartmann Ferenc <ferenc.hartmann@blackswan.com>
2018-04-04 22:17:29 +0200

    todo-react: started to create todo app from youtube tutorial

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import ProductItem from './productItem.js';
+const products = [
+  {
+    name: 'iPad',
+    price: 200
+  },
+  {
+    name: 'iPhone',
+    price: 650
+  }
+]
+
+localStorage.setItem('products', JSON.stringify(products));
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: []
+    };
+  }
+
+  componentWillMount() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    const products = JSON.parse(localStorage.getItem('products'));
+    this.setState({ products });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Products Manager</h1>
+
+      {
+        this.state.products.map(product => {
+          return (
+            <ProductItem
+              key={product.name}
+              {...product}
+            />
+          );
+        })
+      }
+      </div>
+    );
+  }
+}
+
+export default App;
